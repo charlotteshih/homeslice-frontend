0e721c33e9a8e43a7cc9fb049a0dd57cc0c85c29
ryancahela <ryancahela@gmail.com>
2019-08-06 16:55:36 -0700

    form submits successfully to backend

--- src/components/CreateAccount/createAccount.js
+++ src/components/CreateAccount/createAccount.js
@@ -1,35 +1,112 @@
-import React, {useState, useEffect, useContext} from 'react'
+import React, {useState, useEffect, useContext} from 'react';
+import GlobalContext from '../../contexts/GlobalContext';
+import Config from '../../config';
 
-export default function CreateAccount() {
+export default function CreateAccount({ history }) {
+  const formStyle = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "flex-start",
+  }
+  const pageStyle = {
+    margin: "0 auto",
+    width: "800px"
+  }
+
+  let [restaurantName, setRestaurantName] = useState('');
+  let [email, setEmail] = useState('');
+  let [password, setPassword] = useState('');
+  let [phone, setPhone] = useState('');
+  let [streetAddress, setStreetAddress] = useState('');
+  let [city, setCity] = useState('');
+  let [state, setState] = useState('');
+  let [zipcode, setZipcode] = useState('');
+
+  let context = useContext(GlobalContext);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    _submitCreateAccount({
+      name: restaurantName,
+      email,
+      password,
+      phone,
+      street_address: streetAddress,
+      city,
+      state,
+      zipcode
+    });
+
+    function _submitCreateAccount(formData) {
+      fetch(`${Config.apiBaseUrl}/api/restaurants`, {
+        method: 'POST',
+        body: JSON.stringify(formData),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      .then(res => res.json())
+      .then(response => {
+        context.restaurant = {...response};
+        history.push(`/restaurant/${context.restaurant.id}`)
+      })
+      .catch(err => console.error(err));
+    }
+  }
+
 
   return (
-    <div>
+    <div style={pageStyle}>
       <h1>Create Account</h1>
       <hr />
-      <form>
+      <form onSubmit={handleSubmit} style={formStyle}>
         <label for="restaurantNameInput">Restaurant name</label>
-        <input id="restaurantNameInput" type="text"/>
+        <input 
+          onChange={(e) => setRestaurantName(e.target.value)} 
+          id="restaurantNameInput" 
+          type="text"/>
 
         <label for="emailInput">Email</label>
-        <input id="emailInput" type="text"/>
+        <input 
+          onChange={(e) => setEmail(e.target.value)} 
+          id="emailInput" 
+          type="text"/>
 
         <label for="passwordInput">Password</label>
-        <input id="passwordInput" type="text"/>
+        <input
+          onChange={(e) => setPassword(e.target.value)}  
+          id="passwordInput" 
+          type="text"/>
 
         <label for="phoneInput">Phone</label>
-        <input id="phoneInput" type="text"/>
+        <input 
+          onChange={(e) => setPhone(e.target.value)} 
+          id="phoneInput" 
+          type="text"/>
 
         <label for="streetAddressInput">Street Address</label>
-        <input id="streetAddressInput" type="text"/>
+        <input 
+          onChange={(e) => setStreetAddress(e.target.value)} 
+          id="streetAddressInput" 
+          type="text"/>
 
         <label for="cityInput">City</label>
-        <input id="cityInput" type="text"/>
+        <input 
+          onChange={(e) => setCity(e.target.value)} 
+          id="cityInput" 
+          type="text"/>
 
         <label for="stateInput">State</label>
-        <input id="stateInput" type="text"/>
+        <input 
+          onChange={(e) => setState(e.target.value)} 
+          id="stateInput" 
+          type="text"/>
 
         <label for="zipcodeInput">zipcode</label>
-        <input id="zipcodeInput" type="text"/>
+        <input 
+          onChange={(e) => setZipcode(e.target.value)} 
+          id="zipcodeInput" 
+          ype="text"/>
 
         <input type="submit" value="Create Account" />
       </form>
---
+++ src/config.js
@@ -0,0 +1,6 @@
+const config = {
+  apiBaseUrl: 'http://localhost:8000',
+  clientBaseUrl: 'http://localhost:3000',
+}
+
+export default config;
