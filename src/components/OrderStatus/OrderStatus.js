a10140e2f4bb887165f09d4285b80c78028643a2
ryancahela <ryancahela@gmail.com>
2019-08-07 16:53:09 -0700

    displays basic static order information

--- src/components/OrderStatus/OrderStatus.js
+++ src/components/OrderStatus/OrderStatus.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import GlobalContext from '../../contexts/GlobalContext';
 
 export default function OrderStatus() {
+
+  const context = useContext(GlobalContext);
+
+  const restaurantLocation = 
+    context.RestaurantData.street_address + ", " +
+    context.RestaurantData.city + ", " +
+    context.RestaurantData.state;
+
+
   return (
     <div>
-      orderStatus.js
+      <h1>Order Summary</h1>
+      <p>Order Status: {context.orderData.order_status}</p>
+      <p>Order Total: {context.orderData.order_total}</p>
+      <p>Pickup Location: {restaurantLocation}</p>
     </div>
   )
 }
