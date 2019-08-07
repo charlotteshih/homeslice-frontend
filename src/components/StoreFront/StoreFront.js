82ee2940aee5aa943887e595fd2187a46a62bd22
Charlotte Shih <shih.charlotte@gmail.com>
2019-08-06 22:04:26 -0700

    Finished /restaurant/:restaurantId route

--- src/components/StoreFront/StoreFront.js
+++ src/components/StoreFront/StoreFront.js
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useState, useEffect, useContext } from 'react';
+import { Link } from 'react-router-dom';
+import Config from '../../config';
+import GlobalContext from '../../contexts/GlobalContext';
 
-export default function StoreFront() {
+export default function StoreFront(props) {
+  const { match } = props
+  console.log(match)
+  const context = useContext(GlobalContext)
+  const [restaurant, setRestaurant] = useState({
+    id: 0,
+    name: '',
+    email: '',
+    password: '',
+    phone: '',
+    street_address: '',
+    city: '',
+    state: '',
+    zipcode: ''
+  });
+
+  function fetchRestaurants() {
+    fetch(`${Config.apiBaseUrl}/restaurants/${match.params.restaurantId}`)
+      .then(res => {
+        if(res.status === 200) {
+          return res.json();
+        } else {
+          throw new Error(res);
+        }
+      })
+      .then(resJson => {
+        setRestaurant({...resJson});
+        context.setRestaurantData({...resJson});
+      })
+      .catch(err => console.error(err));
+  }
+
+  useEffect(() => fetchRestaurants(), []);
+
   return (
     <div>
-      storeFront.js
+      <section className="restaurant-info">
+        <h3>{restaurant.name}</h3>
+        <span>
+          {restaurant.street_address}<br />
+          {restaurant.city}, {restaurant.state} {restaurant.zipcode}<br />
+          {restaurant.phone}<br />
+          {restaurant.email}
+        </span>
+      </section>
+      <Link to="/restaurant/2/orderOnline">Place An Order!</Link>
     </div>
   )
 }
