582635af0774f9024862f965a40a066e801f3c7b
Charlotte Shih <shih.charlotte@gmail.com>
2019-08-13 02:16:33 -0700

    Auto loads customer data to Payment.js if saved in localStorage

--- src/components/Payment/Payment.js
+++ src/components/Payment/Payment.js
@@ -26,12 +26,8 @@ export default function Payment({ match, history }) {
 
   const context = useContext(GlobalContext);
 
-  // useEffect(() => {
-  //   if (localStorage) {
-  //     localStorage.getItem(customerData);
-  //   }
-  // });
-
+  let savedData = JSON.parse(localStorage.getItem('customerData'));
+  
   function handleSubmit(e) {
     e.preventDefault();
 
@@ -106,7 +102,8 @@ export default function Payment({ match, history }) {
         <input 
           required
           type="text" 
-          id="firstNameInput" 
+          id="firstNameInput"
+          value={savedData.first_name}
           onChange={(e) => setFirstName(e.target.value)}/>
 
         <label htmlFor="lastNameInput">Last Name</label>
@@ -114,6 +111,7 @@ export default function Payment({ match, history }) {
           required
           type="text" 
           id="lastNameInput"
+          value={savedData.last_name}
           onChange={(e) => setLastName(e.target.value)} />
 
         <label htmlFor="emailInput">Email</label>
@@ -121,6 +119,7 @@ export default function Payment({ match, history }) {
           required
           type="text" 
           id="emailInput"
+          value={savedData.email}
           onChange={(e) => setEmail(e.target.value)} />
 
         <label htmlFor="phoneInput">Phone</label>
@@ -128,6 +127,7 @@ export default function Payment({ match, history }) {
           required
           type="text" 
           id="phoneInput"
+          value={savedData.phone}
           onChange={(e) => setPhone(e.target.value)} />
 
         <label htmlFor="streetAddressInput">Street Address</label>
@@ -135,6 +135,7 @@ export default function Payment({ match, history }) {
           required
           type="text" 
           id="streetAddressInput"
+          value={savedData.street_address}
           onChange={(e) => setStreet_Address(e.target.value)} />
 
         <label htmlFor="cityInput">City</label>
@@ -142,12 +143,14 @@ export default function Payment({ match, history }) {
           required
           type="text" 
           id="cityInput"
+          value={savedData.city}
           onChange={(e) => setCity(e.target.value)} />
 
         <label htmlFor="stateInput">State</label>
         <select 
           required
           id="stateInput"
+          value={savedData.state}
           onChange={(e) => setState(e.target.value)}>
             <option value="">Please select a state...</option>
             <option value="AL">Alabama</option>
@@ -208,6 +211,7 @@ export default function Payment({ match, history }) {
           required
           type="text" 
           id="zipcodeInput"
+          value={savedData.zipcode}
           onChange={(e) => setZipcode(e.target.value)} />
         {validationErr
           ? <input type="submit" value="Place Order" disabled />
