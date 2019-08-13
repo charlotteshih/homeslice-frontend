f8133d45acbf5f7fd106ebdddc71d2c6291ce39e
Charlotte Shih <shih.charlotte@gmail.com>
2019-08-12 21:52:08 -0700

    Made dropdown menu for states in all forms

--- src/components/Payment/Payment.js
+++ src/components/Payment/Payment.js
@@ -47,7 +47,6 @@ export default function Payment({ match, history }) {
         throw new Error(res)
       })
       .then(json => {
-        //set customer id in global state
         return context.setCustomerData({...json})
       })
       .then(updatedState => {
@@ -55,11 +54,8 @@ export default function Payment({ match, history }) {
           restaurant_id: match.params.restaurantId,
           pizza_id: updatedState.pizzaData.id,
           customer_id: updatedState.customerData.id,
-          order_status: 'Ordered',
-          // order_total: Number(updatedState.pizzaData.price)
+          order_status: 'Ordered'
         }
-
-        console.log('orderData',orderData);
         return FetchServices._submitCreateOrder(orderData);
       })
       .then(res => {
@@ -69,7 +65,6 @@ export default function Payment({ match, history }) {
         throw new Error(res);
       })
       .then(json => {
-        console.log('json', json);
         return context.setOrderData({...json})
       })
       .then(updatedState => {
@@ -84,48 +79,107 @@ export default function Payment({ match, history }) {
       <form style={formStyle} onSubmit={handleSubmit}>
         <label htmlFor="firstNameInput">First Name</label>
         <input 
+          required
           type="text" 
           id="firstNameInput" 
           onChange={(e) => setFirstName(e.target.value)}/>
 
         <label htmlFor="lastNameInput">Last Name</label>
         <input 
+          required
           type="text" 
           id="lastNameInput"
           onChange={(e) => setLastName(e.target.value)} />
 
         <label htmlFor="emailInput">Email</label>
         <input 
+          required
           type="text" 
           id="emailInput"
           onChange={(e) => setEmail(e.target.value)} />
 
         <label htmlFor="phoneInput">Phone</label>
         <input 
+          required
           type="text" 
           id="phoneInput"
           onChange={(e) => setPhone(e.target.value)} />
 
         <label htmlFor="streetAddressInput">Street Address</label>
         <input 
+          required
           type="text" 
           id="streetAddressInput"
           onChange={(e) => setStreet_Address(e.target.value)} />
 
         <label htmlFor="cityInput">City</label>
         <input 
+          required
           type="text" 
           id="cityInput"
           onChange={(e) => setCity(e.target.value)} />
 
         <label htmlFor="stateInput">State</label>
-        <input 
-          type="text" 
+        <select 
+          required
           id="stateInput"
-          onChange={(e) => setState(e.target.value)} />
+          onChange={(e) => setState(e.target.value)}>
+            <option value="AL">Alabama</option>
+            <option value="AK">Alaska</option>
+            <option value="AZ">Arizona</option>
+            <option value="AR">Arkansas</option>
+            <option value="CA">California</option>
+            <option value="CO">Colorado</option>
+            <option value="CT">Connecticut</option>
+            <option value="DE">Delaware</option>
+            <option value="DC">District Of Columbia</option>
+            <option value="FL">Florida</option>
+            <option value="GA">Georgia</option>
+            <option value="HI">Hawaii</option>
+            <option value="ID">Idaho</option>
+            <option value="IL">Illinois</option>
+            <option value="IN">Indiana</option>
+            <option value="IA">Iowa</option>
+            <option value="KS">Kansas</option>
+            <option value="KY">Kentucky</option>
+            <option value="LA">Louisiana</option>
+            <option value="ME">Maine</option>
+            <option value="MD">Maryland</option>
+            <option value="MA">Massachusetts</option>
+            <option value="MI">Michigan</option>
+            <option value="MN">Minnesota</option>
+            <option value="MS">Mississippi</option>
+            <option value="MO">Missouri</option>
+            <option value="MT">Montana</option>
+            <option value="NE">Nebraska</option>
+            <option value="NV">Nevada</option>
+            <option value="NH">New Hampshire</option>
+            <option value="NJ">New Jersey</option>
+            <option value="NM">New Mexico</option>
+            <option value="NY">New York</option>
+            <option value="NC">North Carolina</option>
+            <option value="ND">North Dakota</option>
+            <option value="OH">Ohio</option>
+            <option value="OK">Oklahoma</option>
+            <option value="OR">Oregon</option>
+            <option value="PA">Pennsylvania</option>
+            <option value="RI">Rhode Island</option>
+            <option value="SC">South Carolina</option>
+            <option value="SD">South Dakota</option>
+            <option value="TN">Tennessee</option>
+            <option value="TX">Texas</option>
+            <option value="UT">Utah</option>
+            <option value="VT">Vermont</option>
+            <option value="VA">Virginia</option>
+            <option value="WA">Washington</option>
+            <option value="WV">West Virginia</option>
+            <option value="WI">Wisconsin</option>
+            <option value="WY">Wyoming</option>
+        </select>
 
         <label htmlFor="zipcodeInput">Zipcode</label>
         <input 
+          required
           type="text" 
           id="zipcodeInput"
           onChange={(e) => setZipcode(e.target.value)} />
