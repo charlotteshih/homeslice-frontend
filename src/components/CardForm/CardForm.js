bb4e753624dd7475272c083e8f8e9b90ed59abef
Charlotte Shih <shih.charlotte@gmail.com>
2019-08-13 21:23:03 -0700

    Setup stripe payment

--- src/components/CardForm/CardForm.js
+++ src/components/CardForm/CardForm.js
@@ -1,38 +1,29 @@
-import React, { Component } from 'react';
-import { CardElement, injectStripe } from 'react-stripe-elements';
-import FetchServices from '../../services/FetchServices';
+import React, { useState } from "react";
+import { CardElement, injectStripe } from "react-stripe-elements";
+import FetchServices from "../../services/FetchServices";
 
-class CardForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      paymentSuccessful: ''
+function CardForm(props) {
+  let [paymentSuccessful, setPaymentSuccessful] = useState("");
+
+  const _submitStripe = e => {
+    e.preventDefault();
+    let { token } = props.stripe.createToken({ name: "Name" });
+    console.log("token", token);
+    let response = FetchServices._makeStripePayment(token);
+    if (response.ok) {
+      setPaymentSuccessful("Payment Successful!");
+      console.log("Purchase Complete!");
     }
-  }
+  };
 
-  render() {
-    const _submitStripe = e => {
-      e.preventDefault();
-      let { token } = this.props.stripe.createToken({ name: "Name" });
-      console.log('token', token);
-      let response = FetchServices._makeStripePayment(token);
-      if (response.ok) {
-        this.setState({
-          paymentSuccessful: 'Payment Successful!'
-        });
-        console.log("Purchase Complete!");
-      }
-    };
-
-    return (
-      <div className="checkout">
-        <p>Please enter your payment information to complete your purchase.</p>
-        <CardElement />
-        <button onClick={e => _submitStripe(e)}>Next</button>
-        {this.state.paymentSuccessful}
-      </div>
-    );
-  }
+  return (
+    <div className="checkout">
+      <p>Please enter your payment information to complete your purchase.</p>
+      <CardElement />
+      <button onClick={e => _submitStripe(e)}>Next</button>
+      {paymentSuccessful}
+    </div>
+  );
 }
 
 export default injectStripe(CardForm);
