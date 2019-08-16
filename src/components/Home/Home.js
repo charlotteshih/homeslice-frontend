a2c7c2b25d7895fad694a5c14e946f242dbcca91
Felix Meyer <phelixm@gmail.com>
2019-08-16 00:45:04 -0500

    change css of Home.js and Storefront.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,28 +1,63 @@
-import React from 'react'
+import React from "react";
 
 export default function Home({ history }) {
   return (
     <div className="Home__container padding-top-60px">
       <section className="Home__info">
-        <p>Introducing <b>HomeSlice</b>: a one-stop pizza shop builder for restaurants and customers. No bloatware, no nonsense -- made by pizza enthusiasts, for pizza enthusiasts.</p>
-        <p>Setup is fast and easy for restaurants! Just provide your credentials and we'll do the rest:</p>
-        <ul>
-          <li><b>Create a menu</b> using our interactive and inuitive menu builder</li>
-          <li><b>View all orders</b> and update them in real time</li>
-          <li><b>Use our analytics tool</b> to gain insights on your bestselling items</li>
-        </ul>
-        <p>Still not sure whether to take the leap? Log in with our demo account and try it out for yourself!</p>
-        <p><b>Email:</b> demo@demo.com<br />
-          <b>Password:</b> Demo123!</p>
+        <p>
+          Introducing <b>HomeSlice</b>: a one-stop pizza shop builder for
+          restaurants and customers. No bloatware, no nonsense -- made by pizza
+          enthusiasts, for pizza enthusiasts.
+        </p>
+        <p>
+          Setup is fast and easy for restaurants! Just provide your credentials
+          and we'll do the rest.
+        </p>
+        <p>
+          Still not sure whether to take the leap? Log in with our demo account
+          and try it out for yourself!
+        </p>
+        <br />
+        <p>
+          <b>Email:</b> demo@demo.com
+          <br />
+          <b>Password:</b> Demo123!
+        </p>
+        <br />
+        <section className="Home__buttons">
+          <button
+            className="btn"
+            onClick={() => history.push("/create-account")}
+          >
+            Get started
+          </button>
+        </section>
       </section>
-      <section className="Home__buttons">
-        <button 
-          className="btn margin-bottom-10px"
-          onClick={() => history.push('/create-account')}>Create seller account</button>
-        <button
-          className="btn" 
-          onClick={() => history.push('/restaurant/1')}>Continue as a pizza buyer</button>
+      <section>
+        <ul className="Home__rest-features">
+          <li className="Home__rest-features__list-item">
+            <div className="Home__rest-features__icons" />
+            <p className="Home__rest-features__text">
+              <b>Create a menu</b>
+              <br /> using our interactive and intuitive menu builder.
+            </p>
+          </li>
+          <li className="Home__rest-features__list-item">
+            <div className="Home__rest-features__icons" />
+            <p className="Home__rest-features__text">
+              <b>View all orders</b>
+              <br /> and update them in real time.
+            </p>
+          </li>
+          <li className="Home__rest-features__list-item">
+            <div className="Home__rest-features__icons" />
+            <p className="Home__rest-features__text">
+              <b>Use our analytics tool</b>
+              <br /> to gain insights on your bestselling items.
+            </p>
+          </li>
+        </ul>
       </section>
     </div>
-  )
+  );
 }
