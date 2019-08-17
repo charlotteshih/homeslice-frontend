6ced22c388d7d6b1e4e5f0cd18e35e7d6658777b
Charlotte Shih <shih.charlotte@gmail.com>
2019-08-16 18:10:43 -0700

    Added fontawesome icons

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,7 @@
 import React from "react";
 import BackgroundImg from '../../images/pizza_bg.jpg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit, faListAlt, faChartBar } from '@fortawesome/free-regular-svg-icons';
 
 export default function Home({ history }) {
   const bgStyle = {
@@ -11,54 +13,53 @@ export default function Home({ history }) {
   return (
     <div className="Home__container padding-top-60px">
       <section className="Home__info" style={bgStyle}>
-        <section className="Home__info--blurb">
-          <h2>
-            Introducing
-          </h2>
-          <h1>HomeSlice!</h1>
-          <p>
-            A one-stop pizza shop builder for restaurants and customers<br />
-            Built by pizza enthusiasts for pizza enthusiasts
-          </p>
-          <p>
-            Take us for a spin!
-          </p>
-          <p>
-            <b>Email:</b> demo@demo.com
-            <br />
-            <b>Password:</b> Demo123!
-          </p>
-          <section className="Home__buttons">
-            <button
-              className="btn"
-              onClick={() => history.push("/create-account")}
-            >
-              Get Started
-            </button>
+        <section className="Home__info--overlay">
+          <section className="Home__info--blurb">
+            <h2>
+              Introducing
+            </h2>
+            <h1>HomeSlice!</h1>
+            <p>
+              A one-stop pizza shop builder for restaurants and customers<br />
+              Built by pizza enthusiasts for pizza enthusiasts
+            </p>
+            <p>
+              Take us for a spin!
+            </p>
+            <p>
+              <b>Email:</b> demo@demo.com
+              <br />
+              <b>Password:</b> Demo123!
+            </p>
+            <section className="Home__buttons">
+              <button
+                className="btn"
+                onClick={() => history.push("/create-account")}
+              >
+                Get Started
+              </button>
+            </section>
           </section>
         </section>
       </section>
       <section>
         <ul className="Home__rest-features">
           <li className="Home__rest-features__list-item">
-            <div className="Home__rest-features__icons" />
+            <FontAwesomeIcon className="Home__rest-features__icons" icon={faEdit} />
             <p className="Home__rest-features__text">
-              <b>Create a menu</b>
-              <br /> using our interactive and intuitive menu builder.
+              <b>Create a menu</b> using our interactive and intuitive menu builder.
             </p>
           </li>
           <li className="Home__rest-features__list-item">
-            <div className="Home__rest-features__icons" />
+            <FontAwesomeIcon className="Home__rest-features__icons" icon={faListAlt} />
             <p className="Home__rest-features__text">
-              <b>View all orders</b>
-              <br /> and update them in real time.
+              <b>View all orders</b> and update them in real time.
             </p>
           </li>
           <li className="Home__rest-features__list-item">
-            <div className="Home__rest-features__icons" />
+            <FontAwesomeIcon className="Home__rest-features__icons" icon={faChartBar} />
             <p className="Home__rest-features__text">
-              <b>Use our analytics tool</b>
-              <br /> to gain insights on your bestselling items.
+              <b>Use our analytics tool</b> to gain insights on your bestselling items.
             </p>
           </li>
         </ul>
