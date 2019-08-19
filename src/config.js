ba7cc804bb277981b304a62aa508c74e8ba2051f
ryancahela <ryancahela@gmail.com>
2019-08-19 15:42:23 -0700

    fixed stripe error

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 module.exports = {
   API_BASE_URL: 'https://thawing-journey-82234.herokuapp.com/api',
-  REACT_APP_STRIPE_KEY: process.env.REACT_APP_STRIPE_KEY
+  REACT_APP_STRIPE_KEY: "pk_test_FLhGsNBLKIpKSKdr3aroMvjF00XOS0DfdZ"
 };
