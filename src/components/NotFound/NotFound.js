d83b3a6b186f2e302c681ae78674cacac8350d33
ryancahela <ryancahela@gmail.com>
2019-08-22 13:56:33 -0700

    fixed NotFound bug

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -2,16 +2,14 @@ import React from 'react'
 import { Link, Router } from 'react-router-dom';
 import Gif from '../../images/pizz-throw.gif';
 
-export default function NotFound() {
+export default function NotFound({ history }) {
   return (
     <div className="NotFound__container padding-top-60px">
       <h1 className="NotFound__heading">404 Not Found</h1>
       <img className="NotFound__image" src={Gif} alt="404 not found breaking bad pizza throw"/>
       <p className="NotFound__paragraph">Something went wrong click the button to go.</p>
-      <button className="btn">
-        <Router>
-          <Link className="link" to='/'>Home</Link>
-        </Router>
+      <button className="btn" onClick={() => history.push('/')}>
+        Home
       </button>
     </div>
   )
